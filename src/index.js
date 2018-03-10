75cb32f083da68ec6208ba5c67d30c1cb4a32e56
mondSh <mondshein@gmail.com>
2018-03-10 20:57:37 +0200

    module 9 work in progress

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './index.css'
+
 import numeral from 'numeral';
 
-const courseValue = mumeral(1000).format('$0,0.00');
-console.log(`I woukd pay ${courseValue} for this awesome course`);
+const courseValue = numeral(1000).format('$0,0.00');
+
+console.log(`I woukd pay ${courseValue} for this awesome course`); // eslint-disable-line no-console
